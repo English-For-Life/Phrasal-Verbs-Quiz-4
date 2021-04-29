f1d9f202d7300cfa3341a97ff7df62a4ae108fa6
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-04-29 23:21:28 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,103 @@
+//Array of objects
+const quiz = [
+    {
+        q:"My daughter is an early bird. She gets ___ at 7 every day.",
+        options:["up", "by", "on", "off"],
+        answer: 0
+    },
+    {
+     q:"How can Tom get ___ on his poor salary?",
+        options:["off", "by", "down", "through"],
+        answer: 1   
+    },
+    {
+        q:"Pat usually fails to get her jokes ___ .",
+        options:["down", "along", "across", "over"],
+        answer: 2
+    },
+    {
+        q:"Jim got ___ his exams without much effort. ",
+        options:["by", "over", "across", "through"],
+        answer: 3
+    },
+    {
+        q:"You'd better get ___ the back seat. I've got my son's safety seat in the front.",
+        options:["in", "over", "off", "down to"],
+        answer: 0
+    },
+    {
+        q:"I've never got ___ with my brother-in-law.",
+        options:["away", "along", "out", "over"],
+        answer: 1
+    },
+    {
+        q:"If this secret gets ___ , you'll be in trouble.",
+        options:["on", "away", "out", "in"],
+        answer: 2
+    },
+    {
+        q:"We've climbed the fence easily, but now we can't get ___ .",
+        options:["off", "on", "in", "down"],
+        answer: 3
+    },
+    {
+        q:"You should get ___ with your homework right now. ",
+        options:["on", "through", "over", "up"],
+        answer: 0
+    },
+    {
+        q:"Kate hasn't got ___ her separation with Mark.",
+        options:["up", "over", "through", "off"],
+        answer: 1
+    },
+    {
+        q:"The burglar got ___ before we came back home.",
+        options:["off", "out", "away", "over"],
+        answer: 2
+    },
+    {
+        q:"When Moly got ___ the plane, she saw all her family waiting for her.",
+        options:["down to", "through", "out", "off"],
+        answer: 3
+    },
+    {
+        q:"Let's get ___ business.",
+        options:["down to", "on", "up", "over"],
+        answer: 0
+    },
+    {
+        q:"Linda's been unemployed for six months. She's getting ___ thanks to her parents' help.",
+        options:["across", "by", "away", "along"],
+        answer: 1
+    },
+    {
+        q:"After the concert audience got ___ and applauded for ten minutes.",
+        options:["in", "down", "up", "on"],
+        answer: 2
+    },
+    {
+        q:"Do Steve and Jess get ___ ?",
+        options:["up", "out", "away", "along"],
+        answer: 3
+    },
+    {
+        q:"Can you tell me where I should get ___ ?",
+        options:["off", "through", "over", "down to"],
+        answer: 0
+    },
+    {
+        q:"We hope to get ___ for a few days at Easter.",
+        options:["out", "away", "off", "down"],
+        answer: 1
+    },
+    {
+        q:"How did Ann get ___ here? She hasn't got the key, has she?",
+        options:["on", "up", "in", "out"],
+        answer: 2
+    },
+    {
+        q:"It took me two weeks to get ___ the terrible flu.",
+        options:["up", "by", "through", "over"],
+        answer: 3
+    }
+]
